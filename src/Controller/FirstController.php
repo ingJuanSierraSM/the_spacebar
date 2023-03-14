df11b0625ccd9dcfbca7785db261a91717e0f895
ingJuanSierraSM <ing.juan.sierra.sm@gmail.com>
2023-03-14 13:07:39 -0500

    feat(FirstController): add homepage method :sparkles:
    Add homepage method in FirstController to handle requests to the root path '/'

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class FirstController
 {
-
+    public function homepage() 
+    {
+        return new Response("<h1>Hello world!</h1> <br> <h2>By Ssimor</h2>");
+    }
 }
