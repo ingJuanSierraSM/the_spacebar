b579f4334f67ea0be0fa1141baa86b6d0d23c9fe
ingJuanSierraSM <ing.juan.sierra.sm@gmail.com>
2023-03-14 17:50:05 -0500

    feat(FirstController): add greet method :sparkles:
    add greet method with the $person parameter in FirstController to handle requests to the path '/greet/{person}'

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -14,4 +14,12 @@ class FirstController
     {
         return new Response("<h1>Hello world!</h1> <br> <h2>By Ssimor</h2>");
     }
+
+    /**
+     * @Route("/greet/{person}")
+     */
+    public function greet($person)
+    {
+        return new Response("<h2> Hi $person!</h2> <br> <h3>Welcome to the Ssimor 'spacebar' </h3>");
+    }
 }
