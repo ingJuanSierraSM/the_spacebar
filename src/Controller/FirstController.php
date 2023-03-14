0da92638b70de302f9abfc7fc85519cbb7daf08a
ingJuanSierraSM <ing.juan.sierra.sm@gmail.com>
2023-03-14 15:38:38 -0500

    refactor(FirstController): migrate route definition '/' :hammer:
    migrate route definition '/' from routes.yaml to Annotation

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage() 
     {
         return new Response("<h1>Hello world!</h1> <br> <h2>By Ssimor</h2>");
